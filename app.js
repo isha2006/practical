63c697a9be9a282fdb7b6ec8b0360daef2763085
isha2006 <114402052+isha2006@users.noreply.github.com>
2024-05-02 03:45:08 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,104 @@
+// function fun(){
+//   console.log(this);
+// }
+// fun();
+
+// let obj={
+//   a:10,
+//   b:20,
+//   fn:function(){
+//     console.log(this);
+//   }
+// }
+// obj.fn();
+
+// let obj = {
+//   a: 20,
+//   fn: () => {
+//     console.log(this);
+//   },
+// };
+
+// let fun = obj.fn;
+// fun();
+
+// let obj = {
+//   a: 20,
+//   fn: function () {
+//     console.log(this);
+//     function sam() {
+//       console.log(this);
+//     }
+//     sam();
+//   },
+// };
+
+// let fun = obj.fn;
+// fun();
+
+// let obj = {
+//   a: 20,
+//   fn: () => {
+//     console.log(this);
+//     function sam() {
+//       console.log(this);
+//     }
+//     sam();
+//   },
+// };
+
+// let fun = obj.fn;
+// fun();
+
+// let obj = {
+//   a: 20,
+//   fn: function () {
+//     console.log(this);
+//     let sam = () => {
+//       console.log(this);
+//     };
+//     sam();
+//   },
+// };
+
+// let fun = obj.fn;
+// fun();
+
+// let obj = {
+//   a: 20,
+//   fn: function () {
+//     console.log(this);
+//     let sam = () => {
+//       console.log(this);
+//     };
+//     sam();
+//   },
+// };
+
+// obj.fn();
+
+// let obj = {
+//   a: 20,
+//   fn: () => {
+//     console.log(this);
+//     let sam = () => {
+//       console.log(this);
+//     };
+//     sam();
+//   },
+// };
+
+// obj.fn();
+
+// let obj = {
+//   a: 20,
+//   fn: () => {
+//     console.log(this);
+//     function sam() {
+//       console.log(this);
+//     }
+//     sam();
+//   },
+// };
+
+// obj.fn();
